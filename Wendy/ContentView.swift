a89f318949b325d7148085254f1052bbe12f8977
melvnl <melvinlim2000@gmail.com>
2022-05-15 13:15:24 +0700

    Initial Commit

---
+++ Wendy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wendy
+//
+//  Created by melvin on 15/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wendy/WendyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WendyApp.swift
+//  Wendy
+//
+//  Created by melvin on 15/05/22.
+//
+
+import SwiftUI
+
+@main
+struct WendyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
